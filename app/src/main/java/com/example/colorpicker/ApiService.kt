e26e8e5f4b99b54280c6ee042a464ea5390bbdba
Tisza Zoltan <tisza.443@gmail.com>
2021-11-30 10:15:54 +0100

    api service added to get name of selected color

---
+++ app/src/main/java/com/example/colorpicker/ApiService.kt
@@ -0,0 +1,88 @@
+package com.example.colorpicker
+
+package com.example.speedometer
+
+
+
+
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.github.kittinunf.fuel.Fuel
+import com.google.android.gms.location.FusedLocationProviderClient
+import kotlinx.coroutines.*
+import org.json.JSONObject
+import org.json.JSONTokener
+import java.net.URL
+
+//Pass Views to handle
+class ApiService: ViewModel() {
+
+
+    private val colorName = MutableLiveData<String>()
+
+    fun getColorNameLive(): LiveData<String>{
+        return colorName
+    }
+
+
+
+
+
+    fun loadData(colorHex:String) {
+
+        viewModelScope.launch {
+            getColor(colorHex)
+
+        }
+    }
+
+    // Async method
+    private suspend fun getColor(colorHex:String) {
+        return withContext(Dispatchers.IO){
+
+
+
+            Fuel.get("https://www.thecolorapi.com/id?hex="+colorHex)
+                .response { request, response, result ->
+                    val (bytes, error) = result
+
+                    if (bytes != null) {
+                        repeat(500)
+                        {
+                            println(it.toString())
+                        }
+                        var jsonObject = JSONTokener(String(bytes)).nextValue() as JSONObject
+                        jsonObject= JSONTokener(jsonObject.get("name").toString()).nextValue() as JSONObject
+
+                        if(jsonObject.has("value")){
+                            var color=jsonObject.get("value").toString()
+                            colorName.postValue(color)
+                        }
+
+
+
+                    }
+
+
+
+
+
+                }
+        }
+    }
+
+
+
+
+
+
+}
